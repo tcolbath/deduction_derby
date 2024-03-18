d261c9ec59619f7920331584981b69c36127b174
tcolbath <trevor.colbath@gmail.com>
2024-03-18 12:49:24 -0400

    draw track

--- src/game.py
+++ src/game.py
@@ -5,7 +5,7 @@ class Game:
         self.__number_of_horses = number_of_horses
         self.__num_spaces = num_spaces
         self.__win = window
-        self.draw_track(num_spaces)
+        self.draw_track()
 
     def new_game(self):
         # place all horses on start
@@ -19,8 +19,8 @@ class Game:
         
         pass
 
-    def draw_track(self, num_spaces, spaces_x=50):
-        # draw a line for the track with ticks at space
+    def draw_track(self):
+        # draw a line for the track
         if self.__win == None:
             return
         side_margin = 100
@@ -29,3 +29,19 @@ class Game:
         end = Point(self.__win.width - side_margin, top_margin)
         track = Line(start, end)
         self.__win.draw_line(track, "black", 5)
+
+        # draw a tick marks for each of the spaces
+        length_of_track = track.p2.x - track.p1.x
+        spaces_x = int(length_of_track / (self.__num_spaces + 2))
+
+        for i in range(0, self.__num_spaces + 3):
+            space_tick_y1 = Point(start.x + (i * spaces_x) + 1, start.y - 2)
+            space_tick_y2 = Point(start.x + (i * spaces_x) + 1, start.y + 10)
+            space_tick = Line(space_tick_y1, space_tick_y2)
+            if i == 1:
+                self.__win.draw_line(space_tick, "green", 4)
+            elif i == self.__num_spaces + 1:
+                self.__win.draw_line(space_tick, "red", 4)
+            else: 
+                self.__win.draw_line(space_tick)
+
